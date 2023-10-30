a18f5051a1b959bdedc91bb56cb4676129cab316
mynameishimal <69321561+mynameishimal@users.noreply.github.com>
2023-10-29 23:04:32 -0400

    Implemented compute_score with sigmoid function and question_weights with entropy

--- assignment1/main.py
+++ assignment1/main.py
@@ -2,8 +2,11 @@
 import json
 import sys
 import os
+import numpy as np
+from scipy.stats import entropy
 
-INPUT_FILE = 'testdata.json' # Constant variables are usually in ALL CAPS
+INPUT_FILE = 'testdata.json'  # Constant variables are usually in ALL CAPS
+
 
 class User:
     def __init__(self, name, gender, preferences, grad_year, responses):
@@ -15,9 +18,42 @@ class User:
 
 
 # Takes in two user objects and outputs a float denoting compatibility
-def compute_score(user1, user2):
-    # YOUR CODE HERE
-    return 0
+
+def compute_score(user1, user2, question_weights):
+    weighted_diff = sum(w * abs(a - b) for w, a, b in zip(question_weights, user1.responses, user2.responses))
+    max_diff = sum(question_weights)
+
+    # Apply a sigmoid function as we discussed in meeting 2 to map the weighted difference to the [0, 1] range
+    normalized_score = 1 / (1 + np.exp(-weighted_diff / max_diff))
+
+    return normalized_score
+
+
+def get_max_ans(users):
+    max_answer_choice = max(max(user.responses) for user in users)
+    return max_answer_choice
+
+# Utilizes entropy weight method to calculate an "inter-question" based weighting system
+def calculate_question_weights(users):
+    num_users = len(users)
+    num_questions = len(users[0].responses)
+    max_answer_choice = get_max_ans(users)
+
+    # Create a 2D array to store answer counts for each question and choice
+    answer_counts = np.zeros((num_questions, max_answer_choice + 1), dtype=int)
+
+    for user in users:
+        for i, response in enumerate(user.responses):
+            answer_counts[i][response] += 1
+
+    # Calculate the entropy for each question
+    question_entropies = [entropy(counts / np.sum(counts)) for counts in answer_counts]
+
+    # Normalize entropies to get question weights
+    max_entropy = max(question_entropies)
+    question_weights = [1 - (entropy / max_entropy) for entropy in question_entropies]
+
+    return question_weights
 
 
 if __name__ == '__main__':
@@ -35,9 +71,11 @@ if __name__ == '__main__':
                             user_obj['responses'])
             users.append(new_user)
 
-    for i in range(len(users)-1):
-        for j in range(i+1, len(users)):
+    q_weights = calculate_question_weights(users)
+
+    for i in range(len(users) - 1):
+        for j in range(i + 1, len(users)):
             user1 = users[i]
             user2 = users[j]
-            score = compute_score(user1, user2)
+            score = compute_score(user1, user2, q_weights)
             print('Compatibility between {} and {}: {}'.format(user1.name, user2.name, score))
